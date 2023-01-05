f5b50304a8fabd5a1373b7bc2fbd0432c778dc24
Michael Vaughan <mgvaughan66@gmail.com>
2023-01-04 23:37:13 -0500

    adding express functionality

--- server/index.js
+++ server/index.js
@@ -23,6 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(express.static('public')); //Added
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -31,6 +32,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../public/index.html"))); //Added
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
