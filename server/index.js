a3b4c293d257b43dd36af345e7ae2cf4c00c70fb
Michael Vaughan <mgvaughan66@gmail.com>
2023-01-04 23:46:32 -0500

    removing functionallity

--- server/index.js
+++ server/index.js
@@ -23,7 +23,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(express.static('public')); //Added
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
@@ -33,7 +32,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../public/index.html"))); //Added
+
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
